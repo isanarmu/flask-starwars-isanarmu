6c88763c52047d7cd29b1e6704a8c46dba8e24ab
Irene <isanarmu@gmail.com>
2026-01-09 15:25:27 +0000

    starwars flask

--- src/models.py
+++ src/models.py
@@ -1,6 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
+from sqlalchemy import String, Boolean, ForeignKey, Integer
 from sqlalchemy.orm import Mapped, mapped_column
+
 
 db = SQLAlchemy()
 
@@ -8,12 +9,36 @@ class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+    username: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+
+class Planet(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(20), nullable=False)
+    climate: Mapped[str] = mapped_column(String(100), nullable=False)
+    terrain: Mapped[str] = mapped_column(String(100), nullable=False)
+    population: Mapped[str] = mapped_column(String(100), nullable=False)
+
+class People(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(20), nullable=False)
+    gender: Mapped[str] = mapped_column(String(100), nullable=False)
+    height: Mapped[str] = mapped_column(String(100), nullable=False)
+    mass: Mapped[str] = mapped_column(String(20), nullable=False)
 
 
+class Favorites(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    userid: Mapped[int] = mapped_column(ForeignKey('user.id'), nullable=False)
+    planetid: Mapped[int] = mapped_column(ForeignKey('planet.id'), nullable=False)    
+    peopleid: Mapped[int] = mapped_column(ForeignKey('people.id'), nullable=False)    
+
+ 
+
+# en algunas cosas pongo str porque a veces puede ser "unknown"    
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
+            "userid": self.userid,
+            "planetid": self.planetid,
+            "peopleid": self.peopleid,
         }
